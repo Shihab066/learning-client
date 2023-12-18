3197c4721924adc1477a0f38cbfb1b8507c8a424
Shihab066 <md.shihab066@gmail.com>
2023-12-18 23:20:21 +0600

    instructor section responsive done

--- src/Pages/Home/PopularInstructor/PopularInstructor.jsx
+++ src/Pages/Home/PopularInstructor/PopularInstructor.jsx
@@ -13,8 +13,8 @@ const PopularInstructor = () => {
     }, [])
     return (
         <div>
-            <h2 className="mt-32 mb-10 text-center text-4xl font-semibold">Popular Instructors</h2>
-            <div className="lg-container grid md:grid-cols-3 gap-y-10">
+            <h2 className="mt-16 lg:mt-20 xl:mt-32 mb-10 text-center xs:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold">Popular Instructors</h2>
+            <div className="lg-container xs:px-2 lg:px-0 grid xs:grid-cols-2 md:grid-cols-3 xs:gap-x-0 lg:gap-x-0 gap-y-10">
                 {instructors.map(instructorData => <ClassCard
                     key={instructorData._id}
                     item={instructorData}

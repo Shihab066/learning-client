54cbe476e8d39cd624563ce6da8175fdff34a638
Shihab066 <md.shihab066@gmail.com>
2024-06-30 00:34:52 +0600

    more functionality added on pagination

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -6,8 +6,7 @@ import useAxiosSecure from '../../hooks/useAxiosSecure';
 import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { Helmet } from 'react-helmet-async';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
 
 const Classes = () => {
     const { user } = useAuth();
@@ -16,13 +15,14 @@ const Classes = () => {
     const navigate = useNavigate();
     const [itemPerPage, setItemPerPage] = useState(6);
     const [currentPage, setCurrentPage] = useState(1);
+
     const { data, isLoading } = useQuery({
         queryKey: ['classes', itemPerPage, currentPage],
         queryFn: async () => {
-            const res = await axios.get(`http://localhost:5000/classes?limit=${itemPerPage}&page=${currentPage}`)
+            const res = await axios.get(`https://learning-info-bd.vercel.app/classes?limit=${itemPerPage}&page=${currentPage}`);
             return res.data;
-        }
-    })
+        },
+    });
 
     const selectClass = (id) => {
         if (!user) {
@@ -36,9 +36,9 @@ const Classes = () => {
                 confirmButtonText: 'Login'
             }).then((result) => {
                 if (result.isConfirmed) {
-                    navigate('/login')
+                    navigate('/login');
                 }
-            })
+            });
         } else {
             axiosSecure.post(`https://learning-info-bd.vercel.app/selectClass/${id}`, { email: email })
                 .then(res => {
@@ -46,81 +46,121 @@ const Classes = () => {
                         Swal.fire({
                             position: 'center',
                             icon: 'success',
-                            title: `Class Selected Successful`,
+                            title: `Class Selected Successfully`,
                             showConfirmButton: false,
                             timer: 1500
-                        })
-                    }
-                    else {
+                        });
+                    } else {
                         Swal.fire({
                             position: 'center',
                             icon: 'error',
                             title: `Class Already Selected`,
-                            text: 'multiple class selection not possible',
+                            text: 'Multiple class selection is not possible',
                             showConfirmButton: false,
                             timer: 2000
-                        })
+                        });
                     }
-                })
+                });
         }
-    }
+    };
+
     const totalItems = data?.classesCount;
-    let totalPages = Math.ceil(totalItems / itemPerPage);
-    if (!isNaN(totalPages)) {
-        totalPages = new Array(totalPages).fill(0);
-    }
+    const totalPages = Math.ceil(totalItems / itemPerPage);
+    const [visiblePages, setVisiblePages] = useState([]);
+
+    useEffect(() => {
+        const totalPagesArr = Array.from({ length: totalPages }, (_, i) => i + 1);
+        setVisiblePages(totalPagesArr.slice(0, 7));
+    }, [totalPages]);
+
+    useEffect(() => {
+        const totalPagesArr = Array.from({ length: totalPages }, (_, i) => i + 1);
+        if (currentPage === visiblePages[visiblePages.length - 1] || (currentPage === visiblePages[0] && !visiblePages.includes(totalPagesArr[2]))) {
+            if (!visiblePages.includes(totalPagesArr[totalPagesArr.length - 1]) || currentPage === visiblePages[0]) {
+                setVisiblePages(totalPagesArr.slice(currentPage - 4, currentPage + 3));
+            }
+        }
+    }, [currentPage, totalPages, visiblePages]);
+
     const handlePageOptions = (event) => {
         setItemPerPage(parseInt(event.target.value));
-    }
-    return (
+    };
 
+    return (
         <div>
             <Helmet>
-                <title>Learning Point_Classes</title>
+                <title>Learning Point | Classes</title>
             </Helmet>
-            <div className='lg-container flex justify-between px-10 py-6'>
-                <h2 className="text-lg font-medium">All Classes</h2>
-                <div className='flex items-center gap-2'>
-                    <p className='font-medium'>Show:</p>
-                    <select className='border rounded bg-base-200 px-1 focus:outline-none' onChange={handlePageOptions}>
-                        <option value={6}>6</option>
-                        <option value={9}>9</option>
-                        <option value={18}>18</option>
-                    </select>
-                </div>
-            </div>
-            {isLoading ? <div className='flex justify-center items-center h-[700px] ' >
-                <span className="loading loading-spinner text-info loading-lg"></span>
-            </div > :
-                <div>
-                    <div className="lg-container grid grid-cols-2 md:grid-cols-3 gap-y-10">
-                        {data?.classes.map(classData => <ClassCard
-                            key={classData._id}
-                            item={classData}
-                            overlay={false}
-                            btn={true}
-                            cardForClass={true}
-                            selectClass={selectClass}
-                        ></ClassCard>)}
-                    </div>
-
-                    {/* pagination button */}
-                    <div className='flex justify-center gap-2 mt-20'>
-                        {
-                            totalPages?.map((page, index) =>
-                                <button
-                                    key={index}
-                                    className={`px-4 py-2 rounded-lg font-medium transition-colors duration-300 ${currentPage === index + 1 ? 'bg-blue-600 hover:bg-blue-700 text-white' : 'bg-gray-200 hover:bg-gray-300 text-gray-700'}`}
-                                    onClick={() => setCurrentPage(index + 1)}>
-                                    {index + 1}
-                                </button>
-                            )
-                        }
-                    </div>
-                </div>
-            }
+            <Header handlePageOptions={handlePageOptions} />
+            {isLoading ? (
+                <LoadingSpinner />
+            ) : (
+                <Content
+                    data={data}
+                    selectClass={selectClass}
+                    currentPage={currentPage}
+                    visiblePages={visiblePages}
+                    setCurrentPage={setCurrentPage}
+                />
+            )}
         </div>
     );
 };
+
+const Header = ({ handlePageOptions }) => (
+    <div className='lg-container flex justify-between px-10 py-6'>
+        <h2 className="text-lg font-medium">All Classes</h2>
+        <div className='flex items-center gap-2'>
+            <p className='font-medium'>Show:</p>
+            <select className='border rounded bg-base-200 px-1 focus:outline-none' onChange={handlePageOptions}>
+                <option value={6}>6</option>
+                <option value={9}>9</option>
+                <option value={18}>18</option>
+            </select>
+        </div>
+    </div>
+);
+
+const LoadingSpinner = () => (
+    <div className='flex justify-center items-center h-[700px]'>
+        <span className="loading loading-spinner text-info loading-lg"></span>
+    </div>
+);
+
+const Content = ({ data, selectClass, currentPage, visiblePages, setCurrentPage }) => (
+    <div>
+        <div className="lg-container grid grid-cols-2 md:grid-cols-3 gap-y-10">
+            {data?.classes.map(classData => (
+                <ClassCard
+                    key={classData._id}
+                    item={classData}
+                    overlay={false}
+                    btn={true}
+                    cardForClass={true}
+                    selectClass={selectClass}
+                />
+            ))}
+        </div>
+        <Pagination
+            currentPage={currentPage}
+            visiblePages={visiblePages}
+            setCurrentPage={setCurrentPage}
+        />
+    </div>
+);
+
+const Pagination = ({ currentPage, visiblePages, setCurrentPage }) => (
+    <div className='flex justify-center gap-2 mt-20'>
+        {visiblePages.map((pageNo) => (
+            <button
+                key={pageNo}
+                className={`px-4 py-2 rounded-lg font-medium transition-colors duration-300 ${currentPage === pageNo ? 'bg-blue-600 hover:bg-blue-700 text-white' : 'bg-gray-200 hover:bg-gray-300 text-gray-700 hover:underline'}`}
+                onClick={() => setCurrentPage(pageNo)}
+            >
+                {pageNo}
+            </button>
+        ))}
+    </div>
+);
 
 export default Classes;

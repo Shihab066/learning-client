5d756d2964ba252c1f89401397bcbff240906b57
Shihab066 <md.shihab066@gmail.com>
2024-11-29 23:26:53 +0600

    update: Add functionality to submit reviews

--- src/DashBoardPages/StudentDashboard/MyReviews/MyReviews.jsx
+++ src/DashBoardPages/StudentDashboard/MyReviews/MyReviews.jsx
@@ -1,43 +1,54 @@
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { getPendingReviews, getStudentReviews } from "../../../services/reviewsService";
+import { addReview, getPendingReviews, getStudentReviews } from "../../../services/reviewsService";
 import useAuth from "../../../hooks/useAuth";
 import generateImageLink from "../../../utils/generateImageLink";
 import GenerateDynamicStar from "../../../components/GenerateDynamicStar/GenerateDynamicStar";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { toastSuccess } from "../../../utils/toastUtils";
 
 const MyReviews = () => {
     const [activeLink, setActiveLink] = useState('1');
+    const [courseIdForReview, setCourseIdForReview] = useState(''); // this state is used in addCourseModal to add review for specific course
 
     return (
-        <section className="w-full overflow-hidden">
-            <div>
-                <h2 className="text-xl font-medium">
-                    My Reviews
-                </h2>
-            </div>
+        <>
+            <section className="w-full overflow-hidden">
+                <div>
+                    <h2 className="text-xl font-medium">
+                        My Reviews
+                    </h2>
+                </div>
 
-            <div className="my-6 pb-3 border-b flex justify-start gap-x-6">
-                <button onClick={() => setActiveLink('1')} className="group text-gray-500 font-medium">
-                    To Be Reviewed
-                    <div className={`${activeLink === '1' ? 'w-full' : 'w-0'} group-hover:w-full h-[2px] duration-300 bg-black`}></div>
-                </button>
+                <div className="my-6 pb-3 border-b flex justify-start gap-x-6">
+                    <button onClick={() => setActiveLink('1')} className="group text-gray-500 font-medium">
+                        To Be Reviewed
+                        <div className={`${activeLink === '1' ? 'w-full' : 'w-0'} group-hover:w-full h-[2px] duration-300 bg-black`}></div>
+                    </button>
 
-                <button onClick={() => setActiveLink('2')} className="group text-gray-500 font-medium">
-                    History
-                    <div className={`${activeLink === '2' ? 'w-full' : 'w-0'} group-hover:w-full h-[2px] duration-300 bg-black`}></div>
-                </button>
-            </div>
-            {
-                activeLink === '1' &&
-                <PendingReviews />
-            }
+                    <button onClick={() => setActiveLink('2')} className="group text-gray-500 font-medium">
+                        History
+                        <div className={`${activeLink === '2' ? 'w-full' : 'w-0'} group-hover:w-full h-[2px] duration-300 bg-black`}></div>
+                    </button>
+                </div>
+                {
+                    activeLink === '1' &&
+                    <PendingReviews
+                        setCourseIdForReview={setCourseIdForReview}
+                    />
+                }
 
-            {
-                activeLink === '2' &&
-                <MyReviewHistory />
-            }
-        </section>
+                {
+                    activeLink === '2' &&
+                    <MyReviewHistory />
+                }
+            </section>
+            <AddReviewModal
+                key={courseIdForReview}
+                courseIdForReview={courseIdForReview}
+                setCourseIdForReview={setCourseIdForReview}
+            />
+        </>
     );
 };
 
@@ -145,7 +156,7 @@ const ReviewCard = ({ data }) => {
     );
 };
 
-const PendingReviews = () => {
+const PendingReviews = ({ setCourseIdForReview }) => {
     const { user } = useAuth();
 
     // Fetch pending reviews data using React Query
@@ -163,6 +174,7 @@ const PendingReviews = () => {
                     <PendingReviewCard
                         key={index}
                         data={reviewData}
+                        setCourseIdForReview={setCourseIdForReview}
                     />
                 ))
             ) : (
@@ -172,8 +184,8 @@ const PendingReviews = () => {
     );
 };
 
-const PendingReviewCard = ({ data }) => {
-    const { courseId, courseName, courseThumbnail, enrollmentDate, instructorName } = data;
+const PendingReviewCard = ({ data, setCourseIdForReview }) => {
+    const { courseId, courseName, courseThumbnail, instructorName } = data;
 
     return (
         <div className="w-full pb-3 border-b flex items-start gap-x-4">
@@ -199,19 +211,100 @@ const PendingReviewCard = ({ data }) => {
                 <p className="text-xs sm:text-sm text-gray-500">{instructorName}</p>
 
                 {/* Review Button for Small Screens */}
-                <button className="flex sm:hidden text-sm text-blue-500">
+                <label onClick={() => { setCourseIdForReview(courseId) }} htmlFor="add-review-modal" className="flex sm:hidden text-sm text-blue-500">
                     Review
-                </button>
+                </label>
             </div>
 
             {/* Review Button for Larger Screens */}
             <div className="hidden sm:flex self-center">
-                <button className="px-4 py-2 text-sm text-white bg-black rounded">
+                <label onClick={() => { setCourseIdForReview(courseId) }} htmlFor="add-review-modal" className="px-4 py-2 text-sm text-white bg-black rounded">
                     Review
-                </button>
+                </label>
             </div>
         </div>
     );
 };
 
+
+const AddReviewModal = ({ courseIdForReview, setCourseIdForReview }) => {
+    const { user } = useAuth();
+    const [rating, setRating] = useState(0);
+    const ratingMessage = ['Select Rating', 'Awful, not what I expected at all', 'Poor, pretty disappointed', 'Average, could be better', 'Good, what I expected', 'Amazing, above expectations!'];
+
+    const handleReviewSubmit = async (e) => {
+        e.preventDefault();
+        const reviewData = {
+            _courseId: courseIdForReview,
+            _studentId: user.uid,
+            userName: user.displayName,
+            userImage: user.photoURL,
+            rating,
+            review: e.target.review.value,            
+        };
+
+        const res = await addReview(reviewData);
+
+        if (res.insertedId) {
+            toastSuccess('Thank you! Your review has been submitted.')
+        }
+
+        
+
+    }
+    return (
+        <div>
+            < input type="checkbox" id="add-review-modal" className="modal-toggle" />
+            <div className="modal" role="dialog">
+                <div className="modal-box w-11/12 max-w-xl py-10">
+                    <div className="flex flex-col items-center">
+                        <h3 className="text-xl font-bold text-center">How would you rate this course?</h3>
+                        <p className="py-4 font-medium">{ratingMessage[rating]}</p>
+                        <div className="rating rating-lg">
+                            <input
+                                type="radio"
+                                name="rating"
+                                className="hidden"
+                                defaultChecked
+                            />
+                            {[1, 2, 3, 4, 5].map((value) => (
+                                <input
+                                    key={value}
+                                    type="radio"
+                                    name="rating"
+                                    className="mask mask-star bg-orange-400"
+                                    checked={rating === value}
+                                    onChange={() => setRating(value)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+
+                    {/* review input */}
+                    {
+                        rating > 0 &&
+                        <form onSubmit={handleReviewSubmit} className="flex flex-col gap-y-4">
+                            <textarea
+                                name="review"
+                                rows="5"
+                                placeholder="Tell us about your own personal experience taking this course. Was it a good match for you?"
+                                className="w-full border border-black focus:outline-none mt-4 px-4 py-3.5 resize-none placeholder-gray-600"
+                            />
+
+                            <input
+                                type="submit"
+                                value='Save and Continue'
+                                className="self-end w-fit font-bold text-white bg-black hover:bg-opacity-80 px-3 py-3 cursor-pointer"
+                            />
+                        </form>
+                    }
+
+                    <div onClick={() => setCourseIdForReview('')} className="modal-action">
+                        <label htmlFor="add-review-modal" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 export default MyReviews;
--- src/services/reviewsService.js
+++ src/services/reviewsService.js
@@ -2,10 +2,15 @@ import api from "./baseAPI"
 
 export const getStudentReviews = async (studentId) => {
     const res = await api.get(`review/my-reviews/${studentId}`);
-    return res.data
+    return res.data;
 };
 
 export const getPendingReviews = async (studentId) => {
     const res = await api.get(`review/pending-reviews/${studentId}`);
-    return res.data
+    return res.data;
+};
+
+export const addReview = async (reviewData) => {
+    const res = await api.post(`review/add`, reviewData);
+    return res.data;
 };

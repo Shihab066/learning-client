1fbd1a096087f66c85197d2e3fc778bb606cccbc
Shihab066 <md.shihab066@gmail.com>
2024-12-25 19:11:27 +0600

    Feature: Added a Manage Users page for admins to view and update user roles.

--- src/DashBoardPages/AdminDashBoard/ManageUser/ManageUser.jsx
+++ src/DashBoardPages/AdminDashBoard/ManageUser/ManageUser.jsx
@@ -1,10 +1,185 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import AdminIcon from "../../../components/Icons/AdminIcon";
+import InstructorIcon from "../../../components/Icons/InstructorIcon";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
+import BlockIcon from "../../../components/Icons/BlockIcon";
+import generateImageLink from "../../../utils/generateImageLink";
+import { useRef, useState } from "react";
+import StudentIcon from "../../../components/Icons/StudentIcon";
+import { toastSuccess } from "../../../utils/toastUtils";
+import Swal from "sweetalert2";
+import searchIcon from '../../../assets/icon/search_icon.svg';
 
 const ManageUser = () => {
+    const { user } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+    const [userInfo, setUserInfo] = useState({});
+
+    const { data: users = [], isloading } = useQuery({
+        queryKey: ['fetch-users'],
+        enabled: !!user,
+        queryFn: async () => {
+            const res = await axiosSecure.get(`http://localhost:5000/api/v1/user/all/${user.uid}`);
+            return res.data;
+        }
+    });
+
+    console.log(users);
     return (
         <div>
-            manage user
+            <div className="border-b pb-2 flex justify-between">
+                <h2 className="text-lg font-bold">Manage User</h2>
+                <div className="min-w-[250px] h-fit relative">
+                    <input
+                        autoComplete='off'
+                        name='search'
+                        type="text"
+                        placeholder="Search User"
+                        className="w-full border py-1.5 rounded-md pl-2 pr-10 focus:outline-none"
+                    />
+                    {/* search icon */}
+                    <button type='submit'>
+                        <img
+                            className='w-6 absolute right-2 top-1/2 -translate-y-1/2'
+                            src={searchIcon}
+                            alt="search icon" />
+                    </button>
+                </div>
+            </div>
+
+            <table className="table">
+                {/* Table Head */}
+                <thead className='hidden md:table-header-group'>
+                    <tr>
+                        <th className='w-[35%]'>User</th>
+                        <th className='w-[25%]'>Email</th>
+                        <th className='w-[15%]'>Role</th>
+                        <th className='w-[25%] min-w-[260px]'>Actions</th>
+                    </tr>
+                </thead>
+                <tbody className="">
+                    {
+                        users.map((data, index) =>
+                            <>
+                                {
+                                    data._id !== user.uid &&
+                                    <UserRow
+                                        key={index}
+                                        data={data}
+                                        setUserInfo={setUserInfo}
+                                    />
+                                }
+                            </>
+                        )
+                    }
+                </tbody>
+            </table>
+            <ChangeRoleModal
+                userInfo={userInfo}
+            />
         </div>
     );
+};
+
+const UserRow = ({ data, setUserInfo }) => {
+    const { name, image, email, role } = data;
+    return (
+        <tr>
+            <td>
+                <div className="flex items-center gap-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                            <img
+                                className="object-cover"
+                                src={generateImageLink({ imageId: image, width: 128 })}
+                                alt="user image" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{name}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {email}
+            </td>
+            <td>
+                <div className="badge badge-ghost">
+                    {role}
+                </div>
+            </td>
+            <th>
+                <div className="flex items-center gap-x-3">
+                    <label onClick={() => setUserInfo(data)} htmlFor="change-role" className="text-sm font-medium bg-black text-white px-2 py-2 rounded-md flex items-center gap-x-1 w-fit cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5" viewBox="0 0 48 48"><g fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth={4}><path d="M18 31h20V5"></path><path d="M30 21H10v22m34-32l-6-6l-6 6"></path><path d="m16 37l-6 6l-6-6"></path></g></svg>
+                        Change Role
+                    </label>
+                    <button className="text-sm font-medium bg-red-500 text-white px-2 py-2 rounded-md flex items-center gap-x-1 w-fit">
+                        <BlockIcon width={5} />
+                        Suspend
+                    </button>
+                </div>
+            </th>
+        </tr>
+    )
+};
+
+const ChangeRoleModal = ({ userInfo }) => {
+    const { _id: userId, role } = userInfo;
+    const [axiosSecure] = useAxiosSecure();
+    const { user } = useAuth();
+    const queryClient = useQueryClient();
+    const closeModalRef = useRef();
+
+    const handleRoleUpdate = (role) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: `Change role to ${role}?`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Confirm"
+        })
+            .then(async (resut) => {
+                if (resut.isConfirmed) {
+                    const res = await axiosSecure.patch(`http://localhost:5000/api/v1/user/role/${user.uid}`, { role, userId });
+                    if (res.data.modifiedCount) {
+                        toastSuccess('User Data updated');
+                        queryClient.refetchQueries(['fetch-users']);
+                        closeModalRef.current.click();
+                    }
+                }
+            });
+    }
+    return (
+        <div>
+            < input type="checkbox" id="change-role" className="modal-toggle" />
+            <div className="modal" role="dialog">
+                <div className="modal-box">
+                    <h3 className="text-lg font-bold">Change Role</h3>
+                    <div className="mt-4 flex justify-center gap-x-4 py-4 select-none">
+                        <button onClick={() => handleRoleUpdate('admin')} className={`text-sm font-medium bg-red-500 text-white px-2 py-2 rounded-md flex items-center gap-x-1 ${role === 'admin' ? 'opacity-50 pointer-events-none' : ''}`}>
+                            <AdminIcon />
+                            Make Admin
+                        </button>
+                        <button onClick={() => handleRoleUpdate('instructor')} className={`text-sm font-medium bg-blue-500 text-white px-2 py-2 rounded-md flex items-center gap-x-1 ${role === 'instructor' ? 'opacity-50 pointer-events-none' : ''}`}>
+                            <InstructorIcon />
+                            Make Instructor
+                        </button>
+                        <button onClick={() => handleRoleUpdate('student')} className={`text-sm font-medium bg-black text-white px-2 py-2 rounded-md flex items-center gap-x-1 ${role === 'student' ? 'opacity-50 pointer-events-none' : ''}`}>
+                            <StudentIcon />
+                            Make Student
+                        </button>
+                    </div>
+                    <div className="modal-action">
+                        <label ref={closeModalRef} htmlFor="change-role" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</label>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
 };
 
 export default ManageUser;

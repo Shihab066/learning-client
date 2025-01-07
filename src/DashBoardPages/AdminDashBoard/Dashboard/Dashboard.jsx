a1dea370735500c6eccdfaedd4d3309aeae14241
Shihab066 <md.shihab066@gmail.com>
2025-01-07 12:27:04 +0600

    Add lifetime sales chart to admin dashboard showing yearly sales amount and monthly breakdown for each year. Now admin can view detailed sales statistics.

--- src/DashBoardPages/AdminDashBoard/Dashboard/Dashboard.jsx
+++ src/DashBoardPages/AdminDashBoard/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import GraphIncrease from "../../../components/Icons/GraphIncrease";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import formatNumber from "../../../utils/FormateNumber";
-import { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
     const [axiosSecure] = useAxiosSecure();
@@ -23,13 +23,34 @@ const Dashboard = () => {
         }
     });
 
+    const { data: totalSalesAmountChartInfo = [] } = useQuery({
+        queryKey: ['total_sales_amount_chart_info'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/dashboard/admin/getTotalSalesAmountChartInfo');
+            return res.data;
+        }
+    });
+
+    // state to manage sales chart
     const [showYearlySalesStats, setShowYearlySalesStats] = useState(false);
     const [salesChartData, setSalesChartData] = useState([]);
     const totalSalesYears = totalSalesChartInfo?.map(data => data.year).reverse();
-    const color = useMemo(() => ['#F2C94C', '#27AE60', '#165DFF'], []);
-    const [randomColor, setRandomColor] = useState('#F2C94C');
     const [currentSalesYear, setCurrentSalesYear] = useState('default');
 
+    // state to manage sales amount chart
+    const [showYearlySalesAmountStats, setShowYearlySalesAmountStats] = useState(true);
+    const [salesAmountChartData, setSalesAmountChartData] = useState([]);
+    const totalSalesAmountYears = totalSalesAmountChartInfo?.map(data => data.year).reverse();
+    const [currentSalesAmountYear, setCurrentSalesAmountYear] = useState('default');
+
+    console.log(currentSalesAmountYear)
+
+    // chart color
+    const color = ['#F2C94C', '#27AE60', '#165DFF', '#10b981', '#8b5cf6', '#fda4af'];
+    const [randomColor, setRandomColor] = useState('#F2C94C');
+    const [randomColor2, setRandomColor2] = useState('#F2C94C');
+
+    // effect for sales chart data
     useEffect(() => {
         if (showYearlySalesStats) {
             const chartData = totalSalesChartInfo?.map(data => ({
@@ -53,7 +74,33 @@ const Dashboard = () => {
             setSalesChartData(monthlyChart);
         }
 
-    }, [totalSalesChartInfo, showYearlySalesStats, color]);
+    }, [totalSalesChartInfo, showYearlySalesStats]);
+
+    // effect for sales amount chart data
+    useEffect(() => {
+        if (showYearlySalesAmountStats) {
+            const chartData = totalSalesAmountChartInfo?.map(data => ({
+                name: data.year,
+                $: data.yearlySalesAmount
+            }));
+
+            setSalesAmountChartData(chartData);
+            setRandomColor2(color[Math.floor(Math.random() * color.length)]);
+        }
+        else {
+            const monthlyChart = [];
+
+            for (let i = 0; i < 12; i++) {
+                const currentMonthData = {
+                    name: `${i + 1}`
+                }
+                totalSalesAmountChartInfo?.forEach(({ year, monthlySalesAmount }) => currentMonthData[`${year + '$'}`] = formatNumber({ num: monthlySalesAmount[i], showFraction: true }));
+                monthlyChart.push(currentMonthData);
+            }
+            setSalesAmountChartData(monthlyChart);
+        }
+
+    }, [totalSalesAmountChartInfo, showYearlySalesAmountStats]);
 
     const totalSalesAmount = formatNumber({ num: totalSalesInfo.totalSalesAmount });
     const thisYearSalesAmount = formatNumber({ num: totalSalesInfo.thisYearSalesAmount });
@@ -174,7 +221,7 @@ const Dashboard = () => {
                 </div>
 
                 {/* chart */}
-                <div className="grow w-full h-[406px] border border-[#E2E8F0] rounded-lg p-4 shadow ">
+                <div className="grow w-full h-[406px] border border-[#E2E8F0] rounded-lg p-4 shadow bg-ro">
                     <div className="mb-4 flex justify-between items-center">
                         <h3 className="font-medium text-xl">Life Time Sales: <span className="text-violet-400">175K</span></h3>
 
@@ -208,6 +255,7 @@ const Dashboard = () => {
                     <div className="h-[calc(100%-44px)]">
                         <ResponsiveContainer width="100%">
                             <AreaChart
+                                key={currentSalesYear + 'sales'}
                                 data={salesChartData}
                             >
                                 {/* <CartesianGrid strokeDasharray="0 1" /> */}
@@ -224,7 +272,7 @@ const Dashboard = () => {
                                                     totalSalesChartInfo?.map((data, index) => {
                                                         if (index !== totalSalesChartInfo.length) {
                                                             return (
-                                                                <>
+                                                                <React.Fragment key={index}>
                                                                     <Area
                                                                         type="linear"
                                                                         dataKey={data.year}
@@ -241,7 +289,7 @@ const Dashboard = () => {
                                                                             <stop offset="100%" stopColor={color[index]} stopOpacity={1} />
                                                                         </linearGradient>
                                                                     </defs>
-                                                                </>
+                                                                </React.Fragment>
                                                             )
                                                         }
                                                     })
@@ -290,69 +338,120 @@ const Dashboard = () => {
                     </div>
                 </div>
             </div>
-            {/* total sales amount */}
+
+            {/* total sales amount chart*/}
             <div className="w-full h-[406px] border border-[#E2E8F0] rounded-lg p-4 mt-10">
-                <h3 className="font-medium text-xl mb-4">Life Time Sales Amount</h3>
+                <div className="mb-4 flex justify-between items-center">
+                    <h3 className="font-medium text-xl">Life Time Sales Amount $: <span className="text-yellow-500">576K</span></h3>
+
+                    <div className="flex items-center gap-x-6">
+                        <div onClick={() => setShowYearlySalesAmountStats(true)} className={`flex items-center gap-x-2 cursor-pointer ${showYearlySalesAmountStats ? 'opacity-100' : 'opacity-60 hover:opacity-90'}`}>
+                            <div className="w-2 h-2 bg-blue-500 rounded-full"></div>
+                            <div className="text-sm">
+                                Yearly
+                            </div>
+                        </div>
+
+                        <div onClick={() => setShowYearlySalesAmountStats(false)} className={`flex items-center gap-x-2 cursor-pointer ${showYearlySalesAmountStats ? 'opacity-60 hover:opacity-90' : 'opacity-100'}`}>
+                            <div className="w-2 h-2 bg-violet-500 rounded-full"></div>
+                            <div className="text-sm">
+                                Monthly
+                            </div>
+                            {
+                                !showYearlySalesAmountStats &&
+                                <select onChange={(e) => setCurrentSalesAmountYear(e.target.value)} value={currentSalesAmountYear} className={`select select-bordered select-xs rounded-sm w-16 ml-1 pr-[20px!important] focus:outline-none bg-[calc(100%-10px)calc(1px+50%),calc(100%-6px)calc(1px+50%)]`}>
+                                    <option value='default'>All</option>
+                                    {
+                                        totalSalesAmountYears?.map((year, index) =>
+                                            <option key={index} value={year}>{year}</option>
+                                        )
+                                    }
+                                </select>
+                            }
+                        </div>
+                    </div>
+                </div>
                 <div className="h-[calc(100%-44px)]">
                     <ResponsiveContainer width="100%">
                         <AreaChart
-                            // width={500}
-                            // height={400}
-                            data={saleAmount}
-                        // syncId="anyId"
-                        // margin={{
-                        //     top: 10,
-                        //     right: 30,
-                        //     left: 0,
-                        //     bottom: 0,
-                        // }}                                
+                            key={currentSalesAmountYear + 'amount'}
+                            data={salesAmountChartData}
                         >
-                            <defs>
-                                <linearGradient id="colorPv" x1="0" y1="1" x2="0" y2="0">
-                                    <stop offset="0%" stopColor="#F2C94C" stopOpacity={0} />
-                                    <stop offset="100%" stopColor="#F2C94C" stopOpacity={1} />
-                                </linearGradient>
-                            </defs>
-                            <defs>
-                                <linearGradient id="colorUv" x1="0" y1="1" x2="0" y2="0">
-                                    <stop offset="0%" stopColor="#27AE60" stopOpacity={0} />
-                                    <stop offset="100%" stopColor="#27AE60" stopOpacity={1} />
-                                </linearGradient>
-                            </defs>
-                            <defs>
-                                <linearGradient id="colorAmt" x1="0" y1="1" x2="0" y2="0">
-                                    <stop offset="0%" stopColor="#165DFF" stopOpacity={0} />
-                                    <stop offset="100%" stopColor="#165DFF" stopOpacity={1} />
-                                </linearGradient>
-                            </defs>
                             {/* <CartesianGrid strokeDasharray="0 1" /> */}
-                            <XAxis dataKey="name" axisLine={{ stroke: '#E2E8F0', strokeWidth: 1 }} tickLine={false} padding={{ left: 30 }} />
+                            <XAxis dataKey="name" axisLine={{ stroke: '#E2E8F0', strokeWidth: 1 }} tickLine={false} padding={{ left: 20 }} />
                             <YAxis axisLine={false} tickLine={false} tickCount={6} width={50} />
                             <Tooltip />
-                            <Area
-                                type="linear"
-                                dataKey="$"
-                                strokeWidth="2"
-                                stroke="#F2C94C"
-                                fill="url(#colorPv)"
-                                stackId="1"
-                            />
-                            {/* <Area
-                                type="linear"
-                                dataKey="uv"
-                                stroke="#27AE60"
-                                fill="url(#colorUv)"
-                                strokeWidth="2"
-                                stackId="1"
-                            />
-                            <Area
-                                type="linear"
-                                dataKey="uv"
-                                stroke="#165DFF"
-                                fill="url(#colorAmt)"
-                                strokeWidth="2"
-                                stackId="1"
-                            /> */}
+                            {
+                                !showYearlySalesAmountStats
+                                    ?
+                                    currentSalesAmountYear === 'default'
+                                        ?
+                                        <>
+                                            {
+                                                totalSalesAmountChartInfo?.map((data, index) => {
+                                                    if (index !== totalSalesAmountChartInfo.length) {
+                                                        return (
+                                                            <React.Fragment key={index}>
+                                                                <Area
+                                                                    type="linear"
+                                                                    dataKey={data.year + '$'}
+                                                                    stroke={color[index]}
+                                                                    fill={`url(#YearlyColor${index})`}
+                                                                    strokeWidth="2"
+                                                                    stackId="1"
+                                                                    className="hover:cursor-pointer"
+                                                                    onClick={() => setCurrentSalesAmountYear(data.year)}
+                                                                />
+                                                                <defs>
+                                                                    <linearGradient id={`YearlyColor${index}`} x1="0" y1="1" x2="0" y2="0">
+                                                                        <stop offset="0%" stopColor={color[index]} stopOpacity={0} />
+                                                                        <stop offset="100%" stopColor={color[index]} stopOpacity={1} />
+                                                                    </linearGradient>
+                                                                </defs>
+                                                            </React.Fragment>
+                                                        )
+                                                    }
+                                                })
+                                            }
+                                        </>
+                                        :
+                                        <>
+                                            <Area
+                                                type="linear"
+                                                dataKey={currentSalesAmountYear + '$'}
+                                                stroke={color[totalSalesChartInfo.findIndex(obj => obj.year === currentSalesAmountYear)]}
+                                                fill={`url(#yearlyColor)`}
+                                                strokeWidth="2"
+                                                stackId="1"
+                                                className="hover:cursor-pointer"
+                                            />
+                                            <defs>
+                                                <linearGradient id={`yearlyColor`} x1="0" y1="1" x2="0" y2="0">
+                                                    <stop offset="0%" stopColor={color[totalSalesChartInfo.findIndex(obj => obj.year === currentSalesAmountYear)]} stopOpacity={0} />
+                                                    <stop offset="100%" stopColor={color[totalSalesChartInfo.findIndex(obj => obj.year === currentSalesAmountYear)]} stopOpacity={1} />
+                                                </linearGradient>
+                                            </defs>
+                                        </>
+                                    :
+                                    <>
+                                        <Area
+                                            type="linear"
+                                            dataKey="$"
+                                            stroke={randomColor2}
+                                            fill={`url(#yearlyColor)`}
+                                            strokeWidth="2"
+                                            stackId="1"
+                                            className="hover:cursor-pointer"
+                                        />
+                                        <defs>
+                                            <linearGradient id={`yearlyColor`} x1="0" y1="1" x2="0" y2="0">
+                                                <stop offset="0%" stopColor={randomColor2} stopOpacity={0} />
+                                                <stop offset="100%" stopColor={randomColor2} stopOpacity={1} />
+                                            </linearGradient>
+                                        </defs>
+                                    </>
+                            }
+
                         </AreaChart>
                     </ResponsiveContainer>
                 </div>

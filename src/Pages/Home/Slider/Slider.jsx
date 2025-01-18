154fe27603467948b90d30017976cc130f1206d8
Shihab066 <md.shihab066@gmail.com>
2025-01-18 23:17:09 +0600

    feat: Update homepage slider
    - Slider images now dynamically fetched from the database.
    - Improved flexibility and maintainability for slider content management.

--- src/Pages/Home/Slider/Slider.jsx
+++ src/Pages/Home/Slider/Slider.jsx
@@ -1,31 +1,42 @@
-
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useQuery } from '@tanstack/react-query';
+import api from '../../../services/baseAPI';
+import generateImageLink from '../../../utils/generateImageLink';
+import getCroppedImg from '../../../utils/getCropImage';
 
-import img1 from '../../../assets/home/1.jpg';
-import img2 from '../../../assets/home/2.jpg';
-import img3 from '../../../assets/home/3.jpg';
-import img4 from '../../../assets/home/4.jpg';
-import img5 from '../../../assets/home/5.jpg';
 
 const Slider = () => {
+    const { data: banners = [], isLoading } = useQuery({
+        queryKey: ["client-banner"],
+        queryFn: async () => {
+            const res = await api.get("/banner/get");
+            const rawBanners = res.data;
+    
+            // Process banners directly in the query function to cache them
+            const bannerImagesPromises = rawBanners.map(async ({ bannerImage, cropArea }) => {
+                const bannerImageUrl = generateImageLink({ imageId: bannerImage });
+                const croppedImage = await getCroppedImg(bannerImageUrl, cropArea);
+                return croppedImage;
+            });
+    
+            const resolvedBannerImages = await Promise.all(bannerImagesPromises);
+            return resolvedBannerImages;
+        },
+        staleTime: Infinity,
+        cacheTime: Infinity,
+        refetchOnWindowFocus: false,
+    });    
+
     return (
         <Carousel className='' animationHandler='fade' swipeable={false} showThumbs={false} showStatus={false} autoPlay={true} infiniteLoop={true} autoFocus={true} interval={4000} transitionTime={500} stopOnHover={true} showArrows={false}>
-            <div>
-                <img src={img1} className='h-full min-h-[400px] object-cover' />
-            </div>
-            <div>
-                <img src={img2} className='h-full min-h-[400px] object-cover' />
-            </div>
-            <div>
-                <img src={img3} className='h-full min-h-[400px] object-cover' />
-            </div>
-            <div>
-                <img src={img4} className='h-full min-h-[400px] object-cover' />
-            </div>
-            <div>
-                <img src={img5} className='h-full min-h-[400px] object-cover' />
-            </div>
+            {
+                banners.map((url, index) => (
+                    <div key={index}>
+                        <img src={url} className='h-full min-h-[25rem] max-h-[45rem] object-cover' />
+                    </div>
+                ))
+            }
         </Carousel>
     );
 };

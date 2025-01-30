fcad02b8f22188ae7b76cc40ccf4de35299e68d0
Shihab066 <md.shihab066@gmail.com>
2025-01-30 16:08:20 +0600

    Enhance navbar profile hover menu: - Added more user details, including name, profile image, and email. - Updated navigation links to be role-based for better user experience.

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -281,14 +281,16 @@ const Navbar = () => {
   // Handle sticky navigation on scroll
   const [stickyNav, setStickyNav] = useState(false);
   useEffect(() => {
-    const handleScroll = () => {
-      setStickyNav(window.scrollY > 800);
-    };
-    window.addEventListener("scroll", handleScroll);
+    if (window.innerWidth >= 1280) {
+      const handleScroll = () => {
+        setStickyNav(window.scrollY > 800);
+      };
+      window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }
   }, []);
 
   // Search functionality
@@ -462,18 +464,95 @@ const Navbar = () => {
                         </li>
 
                         {/* Child div that shows on hover and is interactive */}
-                        <ul className="border-t absolute mt-4 top-full -right-4 w-52 h-fit space-y-1 p-2 bg-white opacity-0 invisible group-hover:opacity-100 group-hover:visible duration-300 z-[9999] pointer-events-auto">
-                          <li className="p-1 rounded text-center hover:bg-black hover:text-white duration-300">
-                            <button onClick={() => navigate(`/user/${isAdmin ? 'dashboard' : isInstructor ? 'instructor_dashboard' : isStudent ? 'profile' : ''}`)}>
-                              Manage account
-                            </button>
-                          </li>
-                          <hr />
-                          <li className="p-1 rounded text-center hover:bg-black hover:text-white duration-300">
-                            <button onClick={signOut}>
-                              Logout
-                            </button>
-                          </li>
+                        <ul className="absolute top-full right-0 opacity-0 invisible group-hover:opacity-100 group-hover:visible duration-300 z-[9999] pointer-events-auto pb-20">
+                          <div className="w-[17.5rem] h-[calc(100vh-6rem)] xl:h-fit space-y-1 bg-white pb-4 rounded mt-4 border-t overflow-y-auto">
+                            {
+                              user &&
+                              <div className="relative px-4 py-4 flex items-center gap-x-3">
+                                {
+                                  user?.photoURL
+                                    ?
+                                    <img
+                                      className="min-w-[4rem] w-16 h-16 rounded-full object-cover bg-white"
+                                      src={generateImageLink({ imageId: user.photoURL, width: 128 })}
+                                      alt="user image"
+                                    />
+                                    :
+                                    <div className="min-w-[3.5rem] h-14 rounded-full text-white bg-black text-lg font-medium flex items-center justify-center">
+                                      {getFirstTwoInitials(user?.displayName)}
+                                    </div>
+                                }
+
+                                {/* user name */}
+                                <div className="font-bold flex flex-col overflow-hidden">
+                                  {user?.displayName}
+                                  <span className="font-normal text-xs text-gray-500 truncate" title={user.email}>{user.email}</span>
+                                </div>
+                              </div>
+                            }
+                            <hr className="border-black opacity-20" />
+                            <div className="menu px-0 py-0 text-base">
+                              {
+                                currentMobileNavItems.map((item, index) => {
+                                  return (
+                                    <div key={index} className={`border-b border-black border-opacity-20 ${currentMobileNavItems.length > 1 ? 'py-1.5' : ''} `}>
+                                      {
+                                        item.category &&
+                                        <h2 className="px-4 text-sm font-bold text-slate-500">
+                                          {item.category}
+                                        </h2>
+                                      }
+                                      <ul>
+                                        {
+                                          item.items.map(item => (
+                                            <li key={item.path}>
+                                              <Link to={item.path} className="mr-[0px!important] rounded-none">
+                                                {item.title}
+                                              </Link>
+                                            </li>
+                                          ))
+                                        }
+                                      </ul>
+                                    </div>
+                                  )
+                                })
+                              }
+                            </div>
+                            <div className="menu px-0 py-0 text-base">
+                              {
+                                currentDropdownMenuItem.map((item, index) => {
+                                  return (
+                                    <div key={index} className={`${index !== currentDropdownMenuItem.length - 1 ? 'border-b border-black border-opacity-20' : ''} ${currentDropdownMenuItem.length > 1 ? 'py-2' : ''} `}>
+                                      {
+                                        item.category &&
+                                        <h2 className="px-4 text-sm font-bold text-slate-500">
+                                          {item.category}
+                                        </h2>
+                                      }
+                                      <ul>
+                                        {
+                                          item.items.map(item => (
+                                            <li key={item.path}>
+                                              <Link to={item.path} className="mr-[0px!important] rounded-none">
+                                                {item.title}
+                                              </Link>
+                                            </li>
+                                          ))
+                                        }
+                                      </ul>
+                                    </div>
+                                  )
+                                })
+                              }
+                            </div>
+
+                            <div onClick={signOut} className="flex items-center gap-x-1 bg-black text-white mx-2 px-2 py-1 mb-4 rounded cursor-pointer">
+                              <svg xmlns="http://www.w3.org/2000/svg" className="w-5" viewBox="0 0 24 24"><path fill="currentColor" d="M12.232 3.25H9.768c-.813 0-1.469 0-2 .043c-.546.045-1.026.14-1.47.366a3.75 3.75 0 0 0-1.64 1.639c-.226.444-.32.924-.365 1.47c-.043.531-.043 1.187-.043 2v6.464c0 .813 0 1.469.043 2c.045.546.14 1.026.366 1.47a3.75 3.75 0 0 0 1.639 1.64c.444.226.924.32 1.47.365c.531.043 1.187.043 2 .043h2.464c.813 0 1.469 0 2-.043c.546-.045 1.026-.14 1.47-.366a3.75 3.75 0 0 0 1.64-1.639c.226-.444.32-.924.365-1.47c.043-.531.043-1.187.043-2V15a.75.75 0 0 0-1.5 0v.2c0 .852 0 1.447-.038 1.91c-.038.453-.107.714-.207.912c-.216.423-.56.767-.983.983c-.198.1-.459.17-.913.207c-.462.037-1.056.038-1.909.038H9.8c-.852 0-1.447 0-1.91-.038c-.453-.038-.714-.107-.911-.207a2.25 2.25 0 0 1-.984-.983c-.1-.198-.17-.459-.207-.913c-.037-.462-.038-1.057-.038-1.909V8.8c0-.852 0-1.447.038-1.91c.037-.453.107-.714.207-.911a2.25 2.25 0 0 1 .984-.984c.197-.1.458-.17.912-.207c.462-.037 1.057-.038 1.909-.038h2.4c.853 0 1.447 0 1.91.038c.453.037.714.107.912.207c.423.216.767.56.983.984c.1.197.17.458.207.912c.037.462.038 1.057.038 1.909V9a.75.75 0 0 0 1.5 0v-.232c0-.813 0-1.469-.043-2c-.045-.546-.14-1.026-.366-1.47a3.75 3.75 0 0 0-1.639-1.64c-.444-.226-.924-.32-1.47-.365c-.531-.043-1.187-.043-2-.043"></path><path fill="currentColor" d="M12.47 8.47a.75.75 0 1 1 1.06 1.06l-1.72 1.72H20a.75.75 0 0 1 0 1.5h-8.19l1.72 1.72a.75.75 0 1 1-1.06 1.06l-3-3a.75.75 0 0 1 0-1.06z"></path></svg>
+                              <span>
+                                Logout
+                              </span>
+                            </div>
+                          </div>
                         </ul>
                       </ul>
                       :
@@ -508,7 +587,7 @@ const Navbar = () => {
                             ?
                             <img
                               className="min-w-[3.5rem] w-14 h-14 rounded-full object-cover bg-white"
-                              src={generateImageLink({imageId: user.photoURL, width: 128})}
+                              src={generateImageLink({ imageId: user.photoURL, width: 128 })}
                               alt="user image"
                             />
                             :

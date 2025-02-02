ffa4eb4ba7c1073df1dc221b8ea1a2596a16cc9b
Shihab066 <md.shihab066@gmail.com>
2025-02-02 07:34:39 +0600

    Enhance private routes and error handling: - Added userRole verification in private routes for improved access control. - Updated error page to use 'Back to Home' navigation instead of history.back.

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,6 @@
 import { useLottie } from 'lottie-react';
 import errorAnimation from '../../assets/errorAnimation/error404.json'
+import { Link } from 'react-router-dom';
 const ErrorPage = ({text = 'Page Not Found', showButton = true}) => {
     const defaultOptions = {
         loop: true,
@@ -12,15 +13,12 @@ const ErrorPage = ({text = 'Page Not Found', showButton = true}) => {
     };
     const { View } = useLottie(defaultOptions);
 
-    const hanldeBack = () => {
-        history.back(-1);
-    }
     return (
         <div className=' flex justify-center'>
             <div className='text-center'>
                 {View}
                 <p className='text-3xl font-bold mb-7'>{ text }</p>
-                {showButton && <button onClick={hanldeBack} className='btn bg-blue-500 hover:bg-blue-600 text-white'>Go Back</button>}
+                {showButton && <Link to='/' className='btn bg-blue-500 hover:bg-blue-600 text-white'>Back To Home</Link>}
             </div>
         </div>
     );
--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,18 +1,23 @@
 
 import useAuth from '../hooks/useAuth';
 import { Navigate, useLocation } from 'react-router-dom';
+import useUserRole from '../hooks/useUserRole';
+import Loading from '../components/Loading/Loading';
 
-const PrivateRoutes = ({ children }) => {
+const PrivateRoutes = ({ type, children }) => {
     const location = useLocation();
-    const { user,loading } = useAuth();          
-    if (loading) {
-        return <div className='flex justify-center items-center h-[700px] '>
-            <span className="loading loading-spinner text-info loading-lg"></span>
-        </div>
+    const { user, loading } = useAuth();
+    const [userRole, isUserRoleLoading] = useUserRole();
+
+    if (loading || isUserRoleLoading) {
+        return <Loading />
     }
 
-    if (user) {
+    if (user && type === userRole) {
         return children
+    }
+    else if (user && type !== userRole) {
+        return <Navigate to='/error404' />
     }
 
     return <Navigate to='/login' state={{ from: location }} replace></Navigate>

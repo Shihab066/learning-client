13aa5cfedd06532191427e186b6545f6034177c3
Shihab066 <md.shihab066@gmail.com>
2025-02-08 11:39:28 +0600

    Improve slider skeleton responsiveness: - Added responsive design to ensure smooth display across all devices.

--- src/Pages/Home/Slider/SliderSkeleton.jsx
+++ src/Pages/Home/Slider/SliderSkeleton.jsx
@@ -2,20 +2,20 @@ import siteLogo from '/logo.png';
 
 const SliderSkeleton = () => {
     return (
-        <div className='lg-container h-[25rem] grid grid-cols-2 gap-x-10 mt-10 px-6'>
-            <div className='w-full h-full flex items-center'>
-                <div className='w-full flex flex-col gap-y-2 mr-28'>
-                    <div className="h-7 skeleton rounded"></div>
-                    <div className="h-5 skeleton rounded-full"></div>
-                    <div className="h-5 skeleton rounded-full"></div>
-                    <div className="h-5 skeleton rounded-full"></div>
-                    <div className="h-5 skeleton rounded-full"></div>
-                    <div className="h-5 skeleton rounded-full"></div>
-                    <div className="w-[70%] h-5 skeleton rounded-full"></div>
+        <div className='lg-container h-fit lg:h-[20rem] xl:h-[25rem] grid md:grid-cols-2 gap-y-4 lg:gap-x-10 mt-6 sm:mt-10 px-6'>
+            <div className='w-full h-full flex items-center order-2 md:order-1'>
+                <div className='w-full flex flex-col gap-y-2 md:mr-10 lg:mr-28'>
+                    <div className="h-5 sm:h-7 skeleton rounded"></div>
+                    <div className="h-3 sm:h-5 skeleton rounded-full"></div>
+                    <div className="h-3 sm:h-5 skeleton rounded-full"></div>
+                    <div className="h-3 sm:h-5 skeleton rounded-full"></div>
+                    <div className="h-3 sm:h-5 skeleton rounded-full"></div>
+                    <div className="h-3 sm:h-5 skeleton rounded-full"></div>
+                    <div className="w-[70%] h-3 sm:h-5 skeleton rounded-full"></div>
                 </div>
             </div>
-            <div className='w-full h-full skeleton rounded-md flex justify-center items-center'>
-                <img className='grayscale opacity-30 w-24' src={siteLogo} alt="" />
+            <div className='w-full h-[12rem] sm:h-[15rem] md:h-full skeleton rounded-md flex justify-center items-center order-1 md:order-2'>
+                <img className='grayscale opacity-30 w-16 sm:w-24' src={siteLogo} alt="" />
             </div>
         </div>
     );
